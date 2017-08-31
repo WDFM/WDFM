c490bd6a9152fb6d4965e1b5936272dadfb3ea9b
WDFM <380733831@qq.com>
2017-08-31 15:04:45 +0800

    2017年8月31日15:04:42
    2017年8月31日15:04:43

--- js/main.js
+++ js/main.js
@@ -7,52 +7,100 @@
 var h = window.screen.height;
 //导航
 var nav = new Vue({
-	el: '.header',
-	data: {
-		height: h,
-		show: true,
-		items: [
-			{ go: '首页' },
-			{ go: '作品' },
-			{ go: '关于' },
-			{ go: '概念' },
-			{ go: '联系' },
-			{ go: '未来'}
-		]
-	},
-	methods: {
-		show: function () {
-			
-		}
-	}
+    el: '.header',
+    data: {
+        height: h,
+        isActive: false,
+        items: [
+            { go: '首页' },
+            { go: '作品' },
+            { go: '关于' },
+            { go: '概念' },
+            { go: '联系' },
+            { go: '未来' }
+        ]
+    },
+    methods: {
+        menuShow: function() {
+            this.isActive = !this.isActive
+        }
+    }
 
 });
 
 //轮播
 var banner = new Vue({
-	el: '#banner',
+    el: '.banner',
+    data: {
+        items: [
+            { enheng: '' },
+            { enheng: '' },
+            { enheng: '' },
+        ],
+        ul: [
+            { img: 'img/banner1.jpg' },
+            { img: 'img/banner2.jpg' },
+            { img: 'img/banner3.jpg' },
+        ],
+        count: 0,
+        timer: '',
+    },
+    created() {
+        this.$nextTick(() => {
+            this.timer = setInterval(() => {
+                this.autoplay()
+            }, 4000)
+        })
+    },
+    methods: {
+        autoplay() {
+            this.count++
+                if (this.count === 3) {
+                    this.count = 0
+                    return
+                }
+        },
+        go() {
+            this.timer = setInterval(this.autoplay, 4000);
+        },
+        prev: function() {
+            this.count--;
+            if (this.count <= 0) {
+                this.count = 0
+                return
+            }
+        },
+        next: function() {
+            this.count++;
+            if (this.count >= 3) {
+                this.count = 0
+                return
+            }
+        },
+        stop: function() {
+            clearInterval(this.timer)
+            this.timer = null
+        },
+        change: function (index) {
+        	this.count = index
+        }
+    }
+});
+
+//about
+var about = new Vue({
+	el: '.about',
 	data: {
-		ul: [
-			{ img: 'img/banner1.jpg'},
-			{ img: 'img/banner2.jpg'},
-			{ img: 'img/banner3.jpg'},
+		items: [
+			{'img':'img/boy.jpg','p':'男生','span':'boy'},
+			{'img':'img/girl.jpg','p':'女生','span':'girl'},
+			{'img':'img/boy.jpg','p':'男生','span':'boy'},
+			{'img':'img/girl.jpg','p':'女生','span':'girl'},
+			{'img':'img/boy.jpg','p':'男生','span':'boy'},
+			{'img':'img/girl.jpg','p':'女生','span':'girl'},
+			{'img':'img/boy.jpg','p':'男生','span':'boy'},
+			{'img':'img/girl.jpg','p':'女生','span':'girl'},
+			{'img':'img/boy.jpg','p':'男生','span':'boy'},
 		],
-		count: 0,
-	},
-	created() {
-		this.go();
-	},
-	methods: {
-		autoplay() {
-			this.count ++
-			if(this.count === 3) {
-				this.count = 0
-				return
-			}
-			// this.ul = _.shuffle(this.ul)
-		},
-		go(){
-			setInterval(this.autoplay, 5000)
-		},
 	}
 })
